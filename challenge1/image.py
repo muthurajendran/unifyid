dd8e03c236e2a9d0cfac912238dc11497446df77
Muthu <raju.spm@gmail.com>
2017-05-08 13:53:42 -0700

    added challenge 2 and readme and requirements

--- challenge1/image.py
+++ challenge1/image.py
@@ -4,10 +4,16 @@ Pulls the random numbers from random.org and creates the image
 import numpy
 from PIL import Image
 import requests
-array = []
-for i in range(0, 128):
-    r = requests.get('https://www.random.org/integers/?num=384&min=0&max=255&col=3&base=10&format=plain&rnd=new')
-    array.append([map(int, i.split('\t')) for i in r.text.splitlines()])
 
-im = Image.fromarray(numpy.array(array).astype('uint8')).convert('RGBA')
-im.save('result_image.png')
+
+def run():
+    array = []
+    for i in range(0, 128):
+        r = requests.get('https://www.random.org/integers/?num=384&min=0&max=255&col=3&base=10&format=plain&rnd=new')
+        array.append([map(int, i.split('\t')) for i in r.text.splitlines()])
+
+    im = Image.fromarray(numpy.array(array).astype('uint8')).convert('RGBA')
+    im.save('result_image.png')
+
+if __name__ == '__main__':
+    run()
--- challenge2/wav.py
+++ challenge2/wav.py
@@ -3,26 +3,27 @@ import struct
 import math
 import requests
 
-array = []
-for i in range(0, 5):
-    r = requests.get('https://www.random.org/integers/?num=9602&min=0&max=255&col=1&base=10&format=plain&rnd=new')
-    array.extend(map(int, r.text.splitlines()))
 
-sampleRate = 16000
-duration = 3.0       # seconds
+def run():
+    array = []
+    for i in range(0, 5):
+        r = requests.get('https://www.random.org/integers/?num=9602&min=0&max=255&col=1&base=10&format=plain&rnd=new')
+        array.extend(map(int, r.text.splitlines()))
 
-# Use different frequencies for the left and right channels
-rFreq = 1760.00  # A
-lFreq = 523.25  # C
+    sampleRate = 16000
+    duration = 3.0       # seconds
 
-wavef = wave.open('sound.wav', 'w')
-wavef.setnchannels(2)  # stereo
-wavef.setsampwidth(2)
-wavef.setframerate(sampleRate)
+    wavef = wave.open('sound.wav', 'w')
+    wavef.setnchannels(2)  # stereo
+    wavef.setsampwidth(2)
+    wavef.setframerate(sampleRate)
 
-print duration * sampleRate
-for i in range(int(duration * sampleRate)):
-    wavef.writeframesraw(struct.pack('<hh', array[i], array[i]))
+    print duration * sampleRate
+    for i in range(int(duration * sampleRate)):
+        wavef.writeframesraw(struct.pack('<hh', array[i], array[i]))
 
-wavef.writeframes('')
-wavef.close()
+    wavef.writeframes('')
+    wavef.close()
+
+if __name__ == "__main__":
+    run()
